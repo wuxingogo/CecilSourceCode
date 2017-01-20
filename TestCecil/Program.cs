7d9e123ba201ef35317197f47ac4c48bd5cd4942
wuxingogo <52111314ly@gmail.com>
2017-01-20 17:35:03 +0800

    command line

--- TestCecil/Program.cs
+++ TestCecil/Program.cs
@@ -4,6 +4,7 @@ using Mono.Cecil.Cil;
 using ICSharpCode.Decompiler;
 using ICSharpCode.Decompiler.Ast;
 using System.IO;
+using System.Collections.Generic;
 
 
 
@@ -13,20 +14,69 @@ namespace TestCecil
 	{
 		public static void Main (string[] args)
 		{
-			MainClass mc = new MainClass ();
+			args = new string[]{ "/Users/wuxingogo/Documents/UnityProject/Casting/OneSideWar/Assets/Plugins/WuxingogoExtension/Plugins/WuxingogoRuntime.dll","-a" };
+			string fullCommand = "";
 			for (int i = 0; i < args.Length; i++) {
-				mc.Decompile (args [i]);
+				fullCommand += args [i] + " ";
+			}
+			Console.WriteLine ("Excute : " + fullCommand);
+			MainClass mc = new MainClass ();
+			int l = args.Length;
+			if (l == 0) {
+				Console.WriteLine ("Argument has Error!");
+				return;
+			} else {
+				List<string> allDll = new List<string> ();
+				for (int i = 0; i < args.Length; i++) {
+					switch (args[i]) {
+					case "-a":
+						mc.DecompileAllDLL (allDll);
+						return;
+					case "-s":
+						List<string> allclass = new List<string> ();
+						for (int j = i + 1; j < args.Length; j++) {
+							allclass.Add (args [j]);
+						}
+						mc.DecompileSingleFile (allDll, allclass);
+						return;
+					default:
+						allDll.Add (args[i]);
+						break;
+					}
+				}
+
+
+
+			}
+
+		}
+
+		void DecompileSingleFile(List<string> allDll,List<string> allClass)
+		{
+			for (int i = 0; i < allDll.Count; i++) {
+				for (int j = 0; j < allClass.Count; j++) {
+					Console.WriteLine ("******* start decompile : " + allDll[i] + "--" + allClass[i] + " *******");
+					DecompileFile (allDll [i], allClass [j]);
+					Console.WriteLine ("******* finish decompile : " + allDll[i] + "--" + allClass[i] + " *******");
+				}
 			}
 		}
 
-		void Decompile(string file)
+		void DecompileAllDLL(List<string> args)
 		{
-			Mono.Cecil.AssemblyDefinition assemblyDefinition = Mono.Cecil.AssemblyDefinition.ReadAssembly (file);
-			AstBuilder astBuilder = null;
+			for (int i = 0; i < args.Count; i++) {
+				Console.WriteLine ("******* start decompile : " + args[i] + " *******");
+				this.DecompileDLL (args [i]);
+				Console.WriteLine ("******* finish decompile : " + args[i] + " *******");
+			}
+		}
 
+		void DecompileFile(string dllName, string className)
+		{
+			Mono.Cecil.AssemblyDefinition assemblyDefinition = Mono.Cecil.AssemblyDefinition.ReadAssembly (dllName);
+			AstBuilder astBuilder = null;
 			foreach (var typeInAssembly in assemblyDefinition.MainModule.Types) {
-				if (typeInAssembly.IsPublic) {
-					Console.WriteLine ("T:{0}", typeInAssembly.Name);
+				if (typeInAssembly.Name == className) {
 					astBuilder = new AstBuilder(new ICSharpCode.Decompiler.DecompilerContext(assemblyDefinition.MainModule) { CurrentType = typeInAssembly } );
 					astBuilder.AddType (typeInAssembly);
 					StringWriter output = new StringWriter ();
@@ -36,6 +86,33 @@ namespace TestCecil
 					using (StreamWriter outputFile = new StreamWriter("Output/" + typeInAssembly.Name +".cs")) {
 						outputFile.Write (result);
 					}
+				}
+			}
+		}
+
+		void DecompileDLL(string dllName)
+		{
+			Mono.Cecil.AssemblyDefinition assemblyDefinition = Mono.Cecil.AssemblyDefinition.ReadAssembly (dllName);
+			AstBuilder astBuilder = null;
+
+			foreach (var typeInAssembly in assemblyDefinition.MainModule.Types) {
+				if (typeInAssembly.IsPublic) {
+					Console.WriteLine ("T:{0}", typeInAssembly.Name);
+
+					try{
+						astBuilder = new AstBuilder(new ICSharpCode.Decompiler.DecompilerContext(assemblyDefinition.MainModule) { CurrentType = typeInAssembly } );
+						astBuilder.AddType (typeInAssembly);
+						StringWriter output = new StringWriter ();
+						astBuilder.GenerateCode (new PlainTextOutput (output));
+						string result = output.ToString ();
+						output.Dispose ();
+						using (StreamWriter outputFile = new StreamWriter("Output/" + typeInAssembly.Name +".cs")) {
+							outputFile.Write (result);
+						}
+					}catch(AssemblyResolutionException e){
+						
+					}
+
 				}
 			}
 		}
